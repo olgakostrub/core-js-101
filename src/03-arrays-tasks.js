b7520bbcda14ffdf7b5cb17885223303099df914
Olga Kostrub <lady.irony.ua@gmail.com>
2020-05-13 02:17:13 +0300

    feat: finish arrays tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -479,7 +479,10 @@ function getIdentityMatrix(n) {
  *     3, 3   => [ 3 ]
  */
 function getIntervalArray(start, end) {
-  return Array.from(new Array(end - start + 1)).map((item, index) => start + index);
+  return Array.from(new Array(end - start + 1)).map(
+    // eslint-disable-next-line comma-dangle
+    (item, index) => start + index
+  );
 }
 
 /**
@@ -527,8 +530,19 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((prevMap, cur) => {
+    const key = keySelector(cur);
+    const value = valueSelector(cur);
+    if (prevMap.has(key)) {
+      const valueArr = prevMap.get(key);
+      valueArr.push(value);
+      prevMap.set(key, valueArr);
+    } else {
+      prevMap.set(key, [value]);
+    }
+    return prevMap;
+  }, new Map());
 }
 
 /**
@@ -544,8 +558,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map((item) => childrenSelector(item)).flat();
 }
 
 /**
@@ -560,8 +574,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((prevArrPart, curIndex) => prevArrPart[curIndex], arr);
 }
 
 /**
@@ -582,8 +596,13 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const middle = Math.floor(arr.length / 2);
+  return [
+    ...[...arr].slice(arr.length % 2 ? middle + 1 : middle),
+    ...(arr.length % 2 ? [arr[middle]] : []),
+    ...[...arr].slice(0, middle),
+  ];
 }
 
 module.exports = {
